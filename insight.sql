90e7c4f8fe9ac66d28b0985007001ce2678a8675
Frank Cano <frank.cano@utec.edu.pe>
2022-04-08 01:14:28 -0500

    CHORE: correction of tables

--- insight.sql
+++ insight.sql
@@ -7,20 +7,20 @@ CREATE TABLE clients (
   age INTEGER,
   gender VARCHAR,
   occupation VARCHAR,
-  nationality VARCHAR,
+  nationality VARCHAR
 );
 
 DROP TABLE IF EXISTS visit_dates;
 CREATE TABLE visit_dates (
   id SERIAL PRIMARY KEY,
   visit_date DATE,
-  client_id INTEGER NOT NULL REFERENCES clients(id),
+  client_id INTEGER NOT NULL REFERENCES clients(id)
 );
 
 DROP TABLE IF EXISTS dishes;
 CREATE TABLE dishes(
   id SERIAL PRIMARY KEY,
-  name VARCHAR,
+  name VARCHAR
 );
 
 DROP TABLE IF EXISTS restaurants;
@@ -29,28 +29,28 @@ CREATE TABLE restaurants(
   name VARCHAR NOT NULL,
   category VARCHAR,
   city VARCHAR,
-  adress VARCHAR,
-)
+  adress VARCHAR
+);
 
 DROP TABLE IF EXISTS prices;
 CREATE TABLE prices(
   id SERIAL PRIMARY KEY,
   price_number INTEGER NOT NULL,
-  dish_id INTEGER NOT NULL
-  FOREIGN KEY (dish_id) REFERENCES dishes(id)
-)
+  dish_id INTEGER NOT NULL REFERENCES dishes(id)
+);
 
 DROP TABLE IF EXISTS restaurants_dishes;
 CREATE TABLE restaurants_dishes(
   id SERIAL PRIMARY KEY,
   restaurant_id INTEGER NOT NULL REFERENCES restaurants(id),
-  dish_id INTEGER NOT NULL REFERENCES dishes(id),
-)
+  dish_id INTEGER NOT NULL REFERENCES dishes(id)
+);
 
 DROP TABLE IF EXISTS restaurants_clients;
 CREATE TABLE restaurants_clients (
   id SERIAL PRIMARY KEY,
   restaurant_id INTEGER NOT NULL REFERENCES restaurants(id),
-  client_id INTEGER NOT NULL REFERENCES clients(id), 
+  client_id INTEGER NOT NULL REFERENCES clients(id)
 );
+
 COMMIT;
